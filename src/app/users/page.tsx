a9504af7590b4de056fc5a41655a1440f9ce8555
farruholimov <farruhbekolimov3904@gmail.com>
2024-07-27 18:56:03 +0500

    update

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -5,6 +5,7 @@ import NotFoundPage from '@/components/site_info/not_found';
 import UsersPage from '../../components/screens/users';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllDesigners, selectAllDesignersStatus } from '../../data/get_all_designers';
+import { selectMyProfile } from '../../data/me';
 
 declare global {
   interface Window {
@@ -20,17 +21,20 @@ export default function Users() {
   const getUserNameFilter = useSelector((state: any) => state?.handle_filters?.users_name)
   const getUsersOrderBy = useSelector((state: any) => state?.handle_filters?.users_orderby)
   const getUsersOrder = useSelector((state: any) => state?.handle_filters?.users_order)
+  const profile = useSelector(selectMyProfile)
 
   React.useEffect(() => {
-    if (getUsersStatus === "idle") {
-      dispatch(getAllDesigners({
-        key: getUserNameFilter,
-        orderBy: getUsersOrderBy,
-        order: getUsersOrder,
-      }))
+    if (profile) {
+      if (getUsersStatus === "idle") {
+        dispatch(getAllDesigners({
+          key: getUserNameFilter,
+          orderBy: getUsersOrderBy,
+          order: getUsersOrder,
+        }))
+      }
     }
   }, [
-    dispatch,
+    profile,
     getUsersStatus,
     getUserNameFilter,
     getUsersOrderBy,
